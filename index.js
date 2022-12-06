e8f8d03ff9b914e45539177496687c5fd226fd18
quantum-insanity <joshstanchek@gmail.com>
2022-12-06 11:19:22 -0500

    base website + index's

---
+++ index.js
@@ -0,0 +1,11 @@
+alert("Welcome to the beta of this website")
+<!--index.js
+
+import name from './games.json';
+console.log(name);
+
+import description from './games.json'
+console.log(description)
+
+import link from './games.json'
+console.log(link)
