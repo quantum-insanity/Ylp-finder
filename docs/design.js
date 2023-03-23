0bb4fcdefe639584b066afe051d6c9f9d88a9835
quantum-insanity <joshstanchek@gmail.com>
2023-03-23 08:53:19 -0400

    added a place holder for design and changed colors for design and engeneering

--- docs/design.js
+++ docs/design.js
@@ -1,16 +1,16 @@
 console.log("Welcome to the beta of this website")
 
-fetch("./games.json")
+fetch("./design.json")
     .then(res => res.json())
     .then(json => {
         var index = 0
-        json.forEach(game => {
+        json.forEach(design => {
             index++
 
             $( "#row" ).prepend(`<div class='column-theme column-${index}'></div>`);
-            $( `.column-${index}` ).append( `<h2>${art.name}</h2>` );
-            $( `.column-${index}` ).append( `<h3>${art.creator}</h3>` );
-            $( `.column-${index}` ).append( `<a href='${game.link}'>Link to ${game.name}</a>` );
+            $( `.column-${index}` ).append( `<h2>${design.name}</h2>` );
+            $( `.column-${index}` ).append( `<h3>${design.creator}</h3>` );
+            $( `.column-${index}` ).append( `<img src="${design.image}" alt="Game Image didnt load = "width="400"; height="300px";"">${design.image}'}</img>` );
         })
     })
     .catch(err => {
