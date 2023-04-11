d19c00e0f377b010f7eaabce33f1fea47c3b8362
quantum-insanity <joshstanchek@gmail.com>
2023-04-11 10:30:06 -0400

    An actual update
    Added clickable images, a placeholder to games with no images and a secret :)

--- docs/games.js
+++ docs/games.js
@@ -10,8 +10,7 @@ fetch("./games.json")
             $( "#row" ).prepend(`<div class='column-theme column-${index}'></div>`);
             $( `.column-${index}` ).append( `<h2>${game.name}</h2>` );
             $( `.column-${index}` ).append( `<h3>${game.description}</h3>` );
-            $( `.column-${index}` ).append( `<a href='${game.link}'>Link to ${game.name}</a>` );
-            $( `.column-${index}` ).append( `<img src="${game.image}" alt="Game Image didnt load = "width="400"; height="300px";"">${game.image}'}</img>` );
+            $( `.column-${index}` ).append( `<a href='${game.link}'><img src="${game.image}" alt="Game Image didnt load = "width="400"; height="300px";""></img></a>` );
 
         })
     })
