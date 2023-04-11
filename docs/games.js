59f608658809feb1ca4933c241f804b2b87d8067
quantum-insanity <joshstanchek@gmail.com>
2023-04-11 12:22:28 -0400

    i swear to god

--- docs/games.js
+++ docs/games.js
@@ -10,7 +10,7 @@ fetch("./games.json")
             $( "#row" ).prepend(`<div class='column-theme column-${index}'></div>`);
             $( `.column-${index}` ).append( `<h2>${game.name}</h2>` );
             $( `.column-${index}` ).append( `<h3>${game.description}</h3>` );
-            $( `.column-${index}` ).append( `<a href='${game.link}'><img src="${game.image}" alt="Game Image didnt load" = "width="400"; height="300px";""></img></a>` );
+            $( `.column-${index}` ).append( `<a href='${game.link}'><img src="${game.image}" alt="Game Image didnt load" style = "width="400"; height="300px";""></img></a>` );
 
         })
     })
