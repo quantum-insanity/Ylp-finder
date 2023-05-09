7c926b5df77f9285d2342266237779d009b5fbde
quantum-insanity <joshstanchek@gmail.com>
2023-05-09 13:06:54 -0400

    updates designs json so that way it will no show no image found and fixed names so it will show

--- docs/design.js
+++ docs/design.js
@@ -1,18 +1,24 @@
 console.log("Welcome to the beta of this website")
 
 fetch("./design.json")
-    .then(res => res.json())
-    .then(json => {
-        var index = 0
-        json.forEach(design => {
-            index++
+  .then(res => res.json())
+  .then(json => {
+    var index = 0
+    json.forEach(design => {
+      index++
+      $( "#row" ).prepend(`<div class='column-theme column-${index}'></div>`);
+      $( `.column-${index}` ).append( `<h2>${design.name}</h2>` );
+      $( `.column-${index}` ).append( `<h3>${design.creator}</h3>` );
 
-            $( "#row" ).prepend(`<div class='column-theme column-${index}'></div>`);
-            $( `.column-${index}` ).append( `<h2>${design.name}</h2>` );
-            $( `.column-${index}` ).append( `<h3>${design.creator}</h3>` );
-            $( `.column-${index}` ).append( `<img src="${design.image}" alt="Image didnt load = "width="400"; height="300px";""></img>` );
-        })
+      // Check if each image property exists before appending it to the HTML
+      for (let i = 1; i <= 7; i++) {
+        const propName = 'image' + i;
+        if (design.hasOwnProperty(propName)) {
+          $( `.column-${index}` ).append( `<img src="${design[propName]}" alt="Image ${propName}" width="400" height="300"></img>` );
+        }
+      }
     })
-    .catch(err => {
-        console.error(err)
-    })
+  })
+  .catch(err => {
+    console.error(err)
+  })
